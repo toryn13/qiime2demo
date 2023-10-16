ecb61851ac98b534fbd6a162658147cf90d84dfd
Toryn Poolman <torynpoolman13@gmail.com>
2023-10-16 14:48:43 +0100

    Update manifest.sh

--- manifest.sh
+++ manifest.sh
@@ -8,19 +8,19 @@ output_manifest="manifest.tsv"
 # Start writing the header to the manifest file
 echo -e "SampleID\tforward-absolute-filepath\treverse-absolute-filepath" > "$output_manifest"
 
+# Loop through all forward FASTQ files and construct the manifest entries
+for forward_file in *.raw_1.fq.gz; do
+    # Check if the corresponding reverse file exists
+    reverse_file="${forward_file/raw_1/raw_2}"
+    if [ -e "$reverse_file" ]; then
+        # Extract the sample name from the file name
+        sample_name=$(basename "$forward_file" .raw_1.fq.gz)
 
-# Loop through all FASTQ.gz files and construct the manifest entries
-for f in *.raw_1.fq.gz; do
-    # Extract the sample name from the file name
-    sample_name=$(echo "$f" | cut -d '.' -f 1)
-
-    # Construct the full file paths
-    forward_seq="${sample_name}.raw_1.fq.gz"
-    reverse_seq="${sample_name}.raw_2.fq.gz"
-
-    # Append the entry to the manifest file
-    echo -e "$sample_name\t$forward_seq\t$reverse_seq" >> "$output_manifest"
+        # Append the entry to the manifest file
+        echo -e "$sample_name\t$forward_file\t$reverse_file" >> "$output_manifest"
+    fi
 done
 
 # Inform the user that the manifest file has been created
 echo "Manifest file '$output_manifest' has been generated."
+
